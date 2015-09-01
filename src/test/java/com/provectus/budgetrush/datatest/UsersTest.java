023f78fa4a7d9cde0ca827ab70a7c3bf1dcde1e6
baevkir <baevkir@gmail.com>
2015-09-01 11:04:59 +0300

    add test

---
+++ src/test/java/com/provectus/budgetrush/datatest/UsersTest.java
@@ -0,0 +1,103 @@
+package com.provectus.budgetrush.datatest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import java.util.Random;
+
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.provectus.budgetrush.data.User;
+import com.provectus.budgetrush.service.UserService;
+import com.provectus.budgetrush.service.UserServiceBean;
+import com.provectus.budgetrush.utils.HibernateConfig;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@DirtiesContext
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { HibernateConfig.class, UserServiceBean.class })
+@WebAppConfiguration
+public class UsersTest {
+    private final String USER_NAME = "Ivan";
+    private final Random random = new Random();
+    @Resource
+    private EntityManagerFactory emf;
+    protected EntityManager em;
+
+    @Autowired
+    private UserService service;
+
+    @Before
+    public void setUp() throws Exception {
+        log.info("Init entity manager");
+        em = emf.createEntityManager();
+    }
+
+    private User saveTestUser() {
+        log.info("Start save test user");
+        User user = new User();
+
+        user.setName(Integer.toString(random.nextInt()));
+        return service.addUser(user);
+    }
+
+    @Test
+    @Transactional
+    public void testSaveUser() throws Exception {
+
+        User user = saveTestUser();
+        assertNotNull(user.getId());
+
+    }
+
+    @Test
+    @Transactional
+    public void testGetAllUsers() throws Exception {
+        log.info("Start get all test");
+        int size = service.getAll().size();
+        saveTestUser();
+        List<User> users = service.getAll();
+        for (User user : users) {
+            log.info("User " + user.getName() + " id " + user.getId());
+        }
+
+        assertNotEquals(size, users.size());
+    }
+
+    @Test
+    @Transactional
+    public void testGetByName() throws Exception {
+        User user = saveTestUser();
+        User user2 = service.getByName(USER_NAME);
+
+        assertEquals(user.getName(), user2.getName());
+        assertEquals(user.getId(), user2.getId());
+        log.info("id1 " + user.getId() + " id2 " + user2.getId());
+    }
+
+    @Test
+    @Transactional
+    public void testGetById() throws Exception {
+        User user = saveTestUser();
+        User user2 = service.getById(user.getId());
+
+        assertEquals(user.getId(), user2.getId());
+        log.info("id1 " + user.getId() + " id2 " + user2.getId());
+    }
+}
