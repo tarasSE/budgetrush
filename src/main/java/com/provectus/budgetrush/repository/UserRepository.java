bef8620d681ea39c3692a4fe36c002ac67e00af6
Konstantin Ramazanov <kramazanov@provectus-it.com>
2015-09-02 08:33:58 -0400

    Review

--- src/main/java/com/provectus/budgetrush/repository/UserRepository.java
+++ src/main/java/com/provectus/budgetrush/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by taras on 28.08.15.
  */
-
+//Annotate accordingly
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByName(String name);
 }
