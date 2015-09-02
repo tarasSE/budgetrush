01ac4a7036da440214b5714315b18afaaa466fb1
baevkir <baevkir@gmail.com>
2015-09-02 23:33:05 +0300

    up

--- src/main/java/com/provectus/budgetrush/data/Currency.java
+++ src/main/java/com/provectus/budgetrush/data/Currency.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
