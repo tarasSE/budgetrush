6e9373ca6023c3e4a39b7252a1cacf057565b5f1
baevkir <baevkir@gmail.com>
2015-09-09 08:02:15 +0300

    delete unused import

--- src/main/java/com/provectus/budgetrush/service/GenericService.java
+++ src/main/java/com/provectus/budgetrush/service/GenericService.java
@@ -1,24 +1,24 @@
 package com.provectus.budgetrush.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public abstract class GenericService <E, R extends JpaRepository<E, Integer>>{
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    public E createAndUpdate(E entity){
+public abstract class GenericService<E, R extends JpaRepository<E, Integer>> {
+
+    public E createAndUpdate(E entity) {
         return getRepository().saveAndFlush(entity);
     }
 
-    public void delete(int id){
+    public void delete(int id) {
         getRepository().delete(id);
     }
 
-    public E getById(int id){
+    public E getById(int id) {
         return getRepository().findOne(id);
     }
 
-    public List<E> getAll(){
+    public List<E> getAll() {
         return getRepository().findAll();
     }
 
