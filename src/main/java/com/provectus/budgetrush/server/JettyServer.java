992063276fb13bca0ae747aa0189c4185b24a93b
Taras Serba <taras.serba@ukr.net>
2015-09-12 04:54:26 +0300

    load resources from WAR

--- src/main/java/com/provectus/budgetrush/server/JettyServer.java
+++ src/main/java/com/provectus/budgetrush/server/JettyServer.java
@@ -8,17 +8,22 @@ import com.google.common.base.Preconditions;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+
 @Slf4j
 @PropertySource("classpath:app.properties")
 class JettyServer implements WebServer {
 
     private static final int DEFAULT_PORT = 8080;
 
-    private static final String WEB_APP_ROOT = "src/main/webapp";
+    private static final String JAR_PATH = JettyServer.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+    private static final String DIR_PATH = new File(JAR_PATH).getParent();
+
+    private static final String WEB_APP_ROOT = "/budgetrush-1.0.0-BUILD-SNAPSHOT.war/";
 
     private static final String CONTEXT_PATH = "/";
 
-    private Server jettyserver;
+    private Server jettyServer;
 
     private int port;
 
@@ -32,11 +37,11 @@ class JettyServer implements WebServer {
         Preconditions.checkState(port != 0, "Port is not specified");
 
         WebAppContext webAppContext = createContext();
-        jettyserver = new Server(port);
-        jettyserver.setHandler(webAppContext);
+        jettyServer = new Server(port);
+        jettyServer.setHandler(webAppContext);
 
         try {
-            jettyserver.start();
+            jettyServer.start();
         } catch (Exception exception) {
             log.error("Failed to start server", exception);
             throw new RuntimeException();
@@ -48,13 +53,13 @@ class JettyServer implements WebServer {
     private WebAppContext createContext() {
         WebAppContext webAppContext = new WebAppContext();
         webAppContext.setContextPath(CONTEXT_PATH);
-        webAppContext.setWar(WEB_APP_ROOT);
+        webAppContext.setWar(DIR_PATH + WEB_APP_ROOT);
         return webAppContext;
     }
 
     @Override
     public void join() throws InterruptedException {
-        jettyserver.join();
+        jettyServer.join();
     }
 
 }
