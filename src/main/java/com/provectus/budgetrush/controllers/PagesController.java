84538fb2f89a3a9bacdd26c45a5145c62ac616f5
Taras Serba <taras.serba@ukr.net>
2015-09-17 18:29:20 +0300

    Add velocity

--- src/main/java/com/provectus/budgetrush/controllers/PagesController.java
+++ src/main/java/com/provectus/budgetrush/controllers/PagesController.java
@@ -3,15 +3,20 @@ package com.provectus.budgetrush.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class PagesController {
 
-    @RequestMapping(value = "/index", method = GET)
-    public String signUp() {
 
+    @RequestMapping(value = "/index", method = GET)
+    public String signUp(Map<String, Object> model) {
+
+        model.put("hello", "Hello world");
         return "index";
     }
+
 
 }
