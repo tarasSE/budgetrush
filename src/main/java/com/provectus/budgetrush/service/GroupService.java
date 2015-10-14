76b322d0df4cd8e6b8ab317745b522e738bb6ccd
Taras Serba <taras.serba@ukr.net>
2015-10-15 00:49:09 +0300

    ref

--- src/main/java/com/provectus/budgetrush/service/GroupService.java
+++ src/main/java/com/provectus/budgetrush/service/GroupService.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.provectus.budgetrush.data.Group;
 import com.provectus.budgetrush.repository.GroupRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class GroupService extends GenericService<Group, GroupRepository> {
 
     @Autowired
