6462d8ad8738bbc21dd6026f709555a67af58c58
taras <taras.serba@ukr.net>
2015-11-26 01:19:22 +0200

    MailSender refactor

--- src/main/java/com/provectus/budgetrush/mail/MailSender.java
+++ src/main/java/com/provectus/budgetrush/mail/MailSender.java
@@ -1,7 +1,9 @@
 package com.provectus.budgetrush.mail;
 
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MailSender extends JavaMailSenderImpl {
 
 }
